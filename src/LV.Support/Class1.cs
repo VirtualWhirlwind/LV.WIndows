42e06d713d4aa0b2060a69238ff9781d78c00b10
kennebel <jac@kennebel.com>
2017-07-06 21:10:44 -0600

    Initial Check In

---
+++ src/LV.Support/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWS.LV
+{
+    public class Class1
+    {
+    }
+}
