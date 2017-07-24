7b1427129ac5542ab0636277abf732aa34f7ac1c
kennebel <jac@kennebel.com>
2017-07-24 17:33:41 -0600

    Start of image layers

--- src/LV.Windows/MainWindow.xaml.cs
+++ src/LV.Windows/MainWindow.xaml.cs
@@ -20,9 +20,49 @@ namespace VWS.LV.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Image> Images { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Images = new List<Image>();
+            TestLoadImages();
+        }
+
+        public void TestLoadImages()
+        {
+            LoadImage(@"C:\Users\Kennebel\Dropbox\Gaming\Factorio\screenshots\20160402162657_1.jpg");
+            LoadImage(@"C:\Users\Kennebel\Dropbox\Gaming\Factorio\screenshots\20160402163226_1.jpg");
+        }
+
+        public void LoadImage(string filename)
+        {
+            Image i = new Image();
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = new Uri(filename, UriKind.Relative);
+            src.CacheOption = BitmapCacheOption.OnLoad;
+            src.EndInit();
+            i.Source = src;
+            i.Stretch = Stretch.Uniform;
+            ImageHolder.Children.Add(i);
+
+            Images.Add(i);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Panel.GetZIndex(Images[0]) == 1)
+            {
+                Panel.SetZIndex(Images[0], 2);
+                Panel.SetZIndex(Images[1], 1);
+            }
+            else
+            {
+                Panel.SetZIndex(Images[0], 1);
+                Panel.SetZIndex(Images[1], 2);
+            }
         }
     }
 }
